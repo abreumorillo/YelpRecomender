e231853c5a5a7ad6b8d4418ee437e58f261e5eb5
Sandy Abreu <abreumorillo@gmail.com>
2016-04-03 20:00:50 -0400

    update documentation

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 #Yelp Restaurant Recommender System
 This System was built for the Knowledge Processing Technology. This is an information retrieval application.
+Namespace The namespace for this application is  YRS, it stands  for Yelp Restaurant System.
 
+Classes should be created  in the src folder.
 #REST API Ready
 #Namespace ready
--- server/src/StemTokenizer.php
+++ server/src/StemTokenizer.php
@@ -1,24 +1,33 @@
-<?php 
+<?php
 
 namespace YRS;
 
 use YRS\PorterStemmer;
-
+/**
+ * @purpose         : This class is responsible for generating the stemmed tokens given a document.
+ * @course          : Knowledge Processing Technologies ISTE-612
+ * @author          : Group
+ * @version         : 1.0
+ */
 class StemTokenizer {
-    
-    
+
+    /**
+     * This method get the stemmed tokens from a given document
+     * @param  String $document
+     * @return Array
+     */
     public static function getTokens($document)
     {
         //"/[\s\"\.,:;&%~^+\(\)\$#!\?\/\\\-]+/";
-        $splitRegex = "/[\s\"\.,:;&%~^+$\(\)\$#!\?\/\\\-]+/";  //Regex for splitting the document  
+        $splitRegex = "/[\s\"\.,:;&%~^+$\(\)\$#!\?\/\\\-]+/";  //Regex for splitting the document
         //Split document into token
         $tokens = preg_split($splitRegex, $document);
         $tokenCount = count($tokens);
         //Tokenize each word
-        for ($i=0; $i < $tokenCount; $i++) { 
+        for ($i=0; $i < $tokenCount; $i++) {
             $tokens[$i] = PorterStemmer::Stem($tokens[$i]);
         }
         return $tokens;
     }
-    
+
 }
